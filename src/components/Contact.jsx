f70fb313058d087e45b72c1257a29068d6430fb7
Shams Minal Rahman <minal3601@gmail.com>
2024-04-29 14:38:53 -0400

    requiring info for contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = () => {
           <p className="text-4xl font-bold inline border-b-4 border-[#ffde3b]">Contact</p>
           <p className="py-4 text-[#dae2e6]">Submit the form below to get in contact :D</p>
         </div>
-        <input className='bg-[#dae2e6] p-2 text-black focus:outline-none' type="text" placeholder='Name' name='name' />
-        <input className='my-4 p-2 bg-[#dae2e6] text-black focus:outline-none' type="email" placeholder='E-Mail' name='email' />
-        <textarea className='bg-[#dae2e6] p-2 text-black focus:outline-none' name='message' rows="10" placeholder='Message'></textarea>
+        <input className='bg-[#dae2e6] p-2 text-black focus:outline-none' type="text" required placeholder='Name' name='name' />
+        <input className='my-4 p-2 bg-[#dae2e6] text-black focus:outline-none' type="email" required placeholder='E-Mail' name='email' />
+        <textarea className='bg-[#dae2e6] p-2 text-black focus:outline-none' name='message' required rows="10" placeholder='Message'></textarea>
         <button className="text-white border-2 px-6 py-3 mt-5 mb-20 mx-auto flex items-center hover:bg-[#ffde3b] hover:text-[#0a192f] hover:border-[#03547b]">Send a message</button>
       </form>
 
